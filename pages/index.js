0cd3848ed5dbc33d9978a3f8ca604c0bbcf342e4
rdd9223 <rdd9223@gmail.com>
2020-07-10 23:10:48 +0900

    refactor: 발표용 준비

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,13 @@ import fetch from "isomorphic-unfetch";
 // as: 우리 URL에 어떻게 보일지 정의
 const ProfileLink = (props) => (
   <div>
-    <Link href={`/p/[profile]`} as={`/p/${props.profile}`}>
+    <Link href={`/user/[profile]`} as={`/user/${props.profile}`}>
       <a>Go to {props.profile}'s profile</a>
     </Link>
   </div>
 );
 
 const Index = (props) => {
-  // return (
-  //   <Layout>
-  //     <h1>Friends List</h1>
-  //     <ProfileLink profile="jake" />
-  //     <ProfileLink profile="peter" />
-  //     <ProfileLink profile="yumi" />
-  //   </Layout>
-  // );
   return (
     <Layout>
       <h1>Friends List {props.profiles[0]}</h1>
@@ -37,7 +29,6 @@ const Index = (props) => {
 Index.getInitialProps = async function () {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
-  console.log(data);
   return {
     profiles: data.map((profile) => profile.name),
   };
